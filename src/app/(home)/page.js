392621f5ee8dfafaf6b16ee59ad1ec59e484a4a8
ibad55252 <ibad55252@gmail.com>
2025-09-08 17:23:22 +0500

    init hero ui

---
+++ src/app/(home)/page.js
@@ -0,0 +1,54 @@
+"use client"
+
+import { MyCustomButton } from '@/components/ui/CustomButton'
+import { Button } from '@heroui/react'
+import { Search01Icon } from 'hugeicons-react'
+
+const HomePage = () => {
+
+  return (
+    <section className='py-10'>
+      <div className='container mx-auto'>
+        <h1 className='text-5xl font-medium'>Home page</h1>
+        <div className='mt-5'>
+
+          <div className='pt-10'>
+            <h2 className='text-4xl'>icon buttons</h2>
+            <div className="flex gap-4 pt-5">
+              <Button size='lg' isIconOnly><Search01Icon size={20} /></Button>
+
+              <MyCustomButton style="primary" size='lg' isIconOnly className={"p-0"}><Search01Icon size={20} /></MyCustomButton>
+              <MyCustomButton isLoading style="primary" size='lg' isIconOnly className={"p-0"}><Search01Icon size={20} /></MyCustomButton>
+            </div>
+          </div>
+
+          <div className='pt-10'>
+            <h2 className='text-4xl'>Custom button primary</h2>
+            <div className="flex gap-4 pt-5">
+              <MyCustomButton isLoading customSize={"xl"} style="primary">Button primary</MyCustomButton>
+              <MyCustomButton customSize={"xl"} style="primary">Button primary</MyCustomButton>
+              <MyCustomButton customSize={"lg"} style="primary">Button primary</MyCustomButton>
+              <MyCustomButton customSize={"md"} style="primary">Button primary</MyCustomButton>
+              <MyCustomButton customSize={"sm"} style="primary">Button primary</MyCustomButton>
+              <MyCustomButton customSize={"xs"} style="primary">Button danger</MyCustomButton>
+            </div>
+          </div>
+
+          <div className='pt-10'>
+            <h2 className='text-4xl'>Custom button danger</h2>
+            <div className="flex gap-4 pt-5">
+              <MyCustomButton customSize={"xl"} style="danger">Button danger</MyCustomButton>
+              <MyCustomButton customSize={"lg"} style="danger">Button danger</MyCustomButton>
+              <MyCustomButton customSize={"md"} style="danger">Button danger</MyCustomButton>
+              <MyCustomButton customSize={"sm"} style="danger">Button danger</MyCustomButton>
+              <MyCustomButton customSize={"xs"} style="danger">Button danger</MyCustomButton>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HomePage
---
+++ src/components/ui/CustomButton.js
@@ -0,0 +1,34 @@
+"use client";
+import { Button } from "@heroui/react";
+import clsx from "clsx";
+
+function customButton({ style = "primary", customSize = "md", className = "" }) {
+
+    // base styles
+    const base = "font-semibold transition-all duration-300";
+
+    // style variants
+    const styleVariants = {
+        primary: "bg-blue-500 hover:bg-blue-600 hover:text-white-500",
+        danger: "bg-red-700 hover:bg-red-900 ",
+    };
+
+    // size variants
+    const sizeVariants = {
+        xs: "px-3 py-2 text-xs",
+        sm: "px-4 py-3.5 text-sm",
+        md: "px-5 py-5.5 text-base",
+        lg: "px-7 py-6.5 text-lg",
+        xl: "px-9 py-7.5 text-xl",
+    };
+
+    return clsx(base, styleVariants[style], sizeVariants[customSize], className);
+}
+
+export function MyCustomButton({ style, customSize, children, className, ...props }) {
+    return (
+        <Button className={customButton({ style, customSize, className })} {...props}>
+            {children}
+        </Button>
+    );
+}
